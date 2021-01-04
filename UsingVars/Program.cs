ac878a7871f21b2c6d81517b16268ed8ec0bbfaf
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-03 18:17:48 -0700

    Add project files.

---
+++ UsingVars/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UsingVars
+{
+    class Program
+    {
+        public class Employee
+        {
+            private int id;
+            private string name;
+
+            public Employee(string Name, int id)
+            {
+                this.name = Name;
+                this.id = id;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            const string openingLine = "Hello, World";
+            Console.WriteLine(openingLine);
+            var secondLine = "With all of COVID-19 flying around everyone is staying inside.";
+            Console.WriteLine(secondLine);
+
+            Employee Joe = new Employee("Joe", 12);
+            Employee Tim = new Employee("Tim", 13);
+        }
+    }
+}
